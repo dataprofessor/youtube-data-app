37f7f8a66d39a2a27df9a8692e35a969aa1bb0f4
Chanin Nantasenamat <51851491+dataprofessor@users.noreply.github.com>
2023-01-09 21:49:47 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,13 @@
+# Import the necessary libraries
 import streamlit as st
+import pandas as pd
 
-st.title('🎈 App Name')
+# User can upload a JSON file
+uploaded_file = st.file_uploader("Choose a JSON file to upload", type="json")
 
-st.write('Hello world!')
+if uploaded_file is not None:
+    # Read the uploaded JSON file and generate a Pandas DataFrame from the JSON data
+    df = pd.read_json(uploaded_file)
+
+    # Display the DataFrame
+    st.dataframe(df)
